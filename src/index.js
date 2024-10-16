09db13ef83540227151d4acbfdfb0740ba310018
Barbaro <barbarogonzalez98@gmail.com>
2024-10-16 15:07:29 -0400

    Completed useEffect

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-
 //import HooksApp from './HooksApp';
 // import CounterApp from './components/useState/CounterApp';
-import CounterWithCustomHook from './components/useState/CounterWithCustomHook';
+// import CounterWithCustomHook from './components/useState/CounterWithCustomHook';
+import { SimpleForm } from './components/useEffect/SimpleForm';
 
 const root = createRoot(document.getElementById('root'));
 root.render(
-    <CounterWithCustomHook/>,
+    <SimpleForm/>,
 );
 
